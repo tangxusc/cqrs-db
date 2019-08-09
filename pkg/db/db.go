2128585cd0ea2861ea93e3f19ad32ae7f5cce6af
tangxu <562050688@qq.com>
2019-08-09 11:39:56 +0800

    修正jdbc无法连接问题

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"github.com/siddontang/go-mysql/mysql"
 	"github.com/siddontang/go-mysql/server"
+	"github.com/siddontang/go-mysql/test_util/test_keys"
 	"github.com/sirupsen/logrus"
 	"github.com/tangxusc/cqrs-db/pkg/config"
 	"net"
@@ -14,16 +16,16 @@ import (
 const serverVersion = "8.0.3"
 
 func Start(ctx context.Context) {
-	l, e := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%s", config.Instance.Db.Port))
+	l, e := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", config.Instance.Db.Port))
 	if e != nil {
 		logrus.Errorf("[db]监听tcp出现错误,错误:%v", e.Error())
 		os.Exit(1)
 	}
-
-	newServer := server.NewServer(serverVersion, mysql.DEFAULT_COLLATION_ID, mysql.AUTH_NATIVE_PASSWORD, nil, nil)
 	provider := server.NewInMemoryProvider()
-	//TODO:在java模式下,提示密码错误,sha1加密问题?
+	//TODO:证书
 	provider.AddUser(config.Instance.Db.Username, config.Instance.Db.Password)
+	var tlsConf = server.NewServerTLSConfig(test_keys.CaPem, test_keys.CertPem, test_keys.KeyPem, tls.VerifyClientCertIfGiven)
+	newServer := server.NewServer(serverVersion, mysql.DEFAULT_COLLATION_ID, mysql.AUTH_SHA256_PASSWORD, test_keys.PubPem, tlsConf)
 	for {
 		select {
 		case <-ctx.Done():
---
+++ pkg/db/db_test.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/siddontang/go-mysql/mysql"
+	"github.com/siddontang/go-mysql/server"
+	"github.com/siddontang/go-mysql/test_util/test_keys"
+	"net"
+	"testing"
+	"time"
+)
+
+type RemoteThrottleProvider struct {
+	*server.InMemoryProvider
+	delay int // in milliseconds
+}
+
+func (m *RemoteThrottleProvider) GetCredential(username string) (password string, found bool, err error) {
+	time.Sleep(time.Millisecond * time.Duration(m.delay))
+	return m.InMemoryProvider.GetCredential(username)
+}
+
+func TestStart(t *testing.T) {
+	l, _ := net.Listen("tcp", "127.0.0.1:3308")
+	// user either the in-memory credential provider or the remote credential provider (you can implement your own)
+	//inMemProvider := server.NewInMemoryProvider()
+	//inMemProvider.AddUser("root", "123")
+	remoteProvider := &RemoteThrottleProvider{server.NewInMemoryProvider(), 10 + 50}
+	remoteProvider.AddUser("root", "123")
+	var tlsConf = server.NewServerTLSConfig(test_keys.CaPem, test_keys.CertPem, test_keys.KeyPem, tls.VerifyClientCertIfGiven)
+	for {
+		c, _ := l.Accept()
+		go func() {
+			// Create a connection with user root and an empty password.
+			// You can use your own handler to handle command here.
+			svr := server.NewServer("8.0.12", mysql.DEFAULT_COLLATION_ID, mysql.AUTH_SHA256_PASSWORD, test_keys.PubPem, tlsConf)
+			conn, err := server.NewCustomizedConn(c, svr, remoteProvider, server.EmptyHandler{})
+
+			if err != nil {
+				fmt.Println("Connection error:", err)
+				return
+			}
+
+			for {
+				conn.HandleCommand()
+			}
+		}()
+	}
+}
